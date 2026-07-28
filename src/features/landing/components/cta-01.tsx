24feffec76a2efae6b3a2280f62272b0830e7b79
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-07-28 13:37:26 +0530

    style(landing-cta): update background, text opacity, and button

--- src/features/landing/components/cta-01.tsx
+++ src/features/landing/components/cta-01.tsx
@@ -11,12 +11,12 @@ export interface Cta01Props {
 
 export function Cta01({ title, description, cta }: Readonly<Cta01Props>) {
   return (
-    <div className="relative w-full overflow-hidden rounded-2xl outline outline-black/10 dark:outline-white/10 py-16 sm:py-20">
+    <div className="relative w-full overflow-hidden rounded-2xl shadow-sm bg-muted py-16 sm:py-20">
       <div className="mx-auto flex max-w-lg flex-col items-center justify-center px-6">
         <div className="flex flex-col items-center space-y-4 self-center">
           <div className="space-y-2">
             {title && (
-              <h2 className="text-center text-3xl font-serif font-normal tracking-tight md:text-4xl md:max-w-200">
+              <h2 className="text-center text-3xl text-foreground/80 font-serif font-normal tracking-tight md:text-4xl md:max-w-200">
                 <Balancer balance={0.5}>{title}</Balancer>
               </h2>
             )}
@@ -26,7 +26,7 @@ export function Cta01({ title, description, cta }: Readonly<Cta01Props>) {
               </p>
             )}
           </div>
-          {cta && <Cta cta={cta} />}
+          {cta && <Cta className="mt-4 border-none shadow-sm bg-accent-foreground/50 hover:bg-accent-foreground/70" cta={cta} />}
         </div>
       </div>
     </div>
