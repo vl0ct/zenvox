6fda9e27a1d509ccd15e5b72ff15fbcbfc481390
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-09-12 23:56:05 +0530

    chore: update voices view

--- src/features/voices/views/voices-view.tsx
+++ src/features/voices/views/voices-view.tsx
@@ -24,11 +24,6 @@ function VoicesContent() {
 export function VoicesView() {
   return (
     <div className="flex min-h-0 flex-1 flex-col">
-      <header className="flex items-center gap-2 border-b px-3 py-2.5 sm:px-4">
-        <span className="truncate text-sm font-medium tracking-tight">
-          Voices
-        </span>
-      </header>
       <div className="flex min-h-0 flex-1 flex-col gap-4 overflow-y-auto p-4 sm:p-5 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
         <VoicesToolbar />
         <VoicesContent />
