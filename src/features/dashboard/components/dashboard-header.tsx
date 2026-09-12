427abbd2cb5e140ab48c5e3e82b677015c85f7ac
Vasudev Jaiswal <vasudevjaiswal050405@gmail.com>
2026-09-12 23:54:19 +0530

    chore: update dashboard header

--- src/features/dashboard/components/dashboard-header.tsx
+++ src/features/dashboard/components/dashboard-header.tsx
@@ -8,7 +8,7 @@ export function DashboardHeader() {
     <div className="flex items-start justify-between">
       <div className="space-y-1 ml-4">
         <p className="text-sm text-muted-foreground">Nice to see you</p>
-        <h1 className="text-xl lg:text-3xl font-sans font-normal tracking-tight">
+        <h1 className="text-xl lg:text-3xl font-sans font-medium tracking-tight">
           {isLoaded ? (user?.fullName ?? user?.firstName ?? "there") : "..."}
         </h1>
       </div>
